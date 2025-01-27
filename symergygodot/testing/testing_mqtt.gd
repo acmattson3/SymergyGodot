97511395ee74af283430ba90b86279a762f08a0f
Andrew Mattson <acmattson3@alaska.edu>
2025-01-27 10:52:38 -0900

    Implemented test program for MQTT; MQTT is working!

---
+++ symergygodot/testing/testing_mqtt.gd
@@ -0,0 +1,21 @@
+extends Control
+
+func _ready():
+	# Connect signals
+	$MQTTHandler.broker_connected.connect(_on_broker_connected)
+	$MQTTHandler.received_message.connect(_on_received_message)
+	$MQTTHandler.broker_connection_failed.connect(_on_broker_connection_failed)
+	
+	# Connect to the broker
+	$MQTTHandler.connect_to_broker("tcp://test.mosquitto.org:1883")
+
+func _on_broker_connected():
+	print("Connected to the MQTT broker.")
+	$MQTTHandler.subscribe("test/topic")
+	$MQTTHandler.publish("test/topic", "Hello from Godot!")
+
+func _on_received_message(topic, message):
+	print("Message received on topic:", topic, "Message:", message)
+
+func _on_broker_connection_failed():
+	print("Failed to connect to the MQTT broker.")
