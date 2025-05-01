71d7b35c89078870f4c7635a20a92fdada85dfa8
Andrew Mattson <acmattson3@alaska.edu>
2025-04-30 17:10:00 -0800

    Removed credits button.

--- symergygodot/ui_elements/menus/main_menu.gd
+++ symergygodot/ui_elements/menus/main_menu.gd
@@ -11,8 +11,5 @@ func _on_start_pressed() -> void:
 func _on_settings_pressed() -> void:
 	$SettingsMenu.show()
 	
-func _on_credits_pressed() -> void:
-	pass # Replace with function body.
-	
 func _on_exit_pressed() -> void:
 	get_tree().quit()
